9020b295cbfe2b44e0837955d00ffbba9a82a13c
andreamazza84 <andrea.mazza84@gmail.com>
2021-02-26 17:06:45 +0100

    Patch su faker immagini

--- database/seeds/ProductsSeeder.php
+++ database/seeds/ProductsSeeder.php
@@ -6,19 +6,23 @@ use Faker\Generator as Faker;
 
 class ProductsSeeder extends Seeder
 {
+   
     /**
      * Run the database seeds.
      *
      * @return void
      */
    
-    public function run(Faker $faker){
-    for ($i=0; $i < 100; $i++) { 
-        $newProd = new Product();
-        $newProd->title = $faker->word();
-        $newProd->body = $faker->sentence(50);
-        $newProd->cover = $faker->imageUrl($width = 640, $height = 480);
-        $newProd->save();
+    public function run(Faker $faker)
+    {
+    $imgfaker = \Faker\Factory::create();
+    $imgfaker->addProvider(new \Xvladqt\Faker\LoremFlickrProvider($imgfaker));
+        for ($i=0; $i < 20; $i++) { 
+            $newProd = new Product();
+            $newProd->title = $faker->word();
+            $newProd->body = $faker->sentence(50);
+            $newProd->cover =  $imgfaker->imageUrl($width="640", $height="480", ['videogames']);
+            $newProd->save();
         }
     }
     
